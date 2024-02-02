7ada0976fc053735f4e55552add469cb2c9762d9
Osamu Uda <uda13@hotmail.co.jp>
2024-02-02 15:35:17 +0900

    create porp-types

--- udemy-react-redux-crud-application/src/App.js
+++ udemy-react-redux-crud-application/src/App.js
@@ -1,13 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes  from 'prop-types';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello,world!</h1>
-      </div>
-    );
-  }
+const App = () => {
+  const profiles = [
+    { name: "taro", age: 10},
+    { name: "hanako", age: 20},
+    { name: "test"}
+  ]
+
+  return (
+    <div>
+      {
+        profiles.map((profile, index) =>{
+           return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
+    </div>
+  )
+}
+
+const User =(props) => {
+  return <div>Hi, I am {props.name}, and {props.age} years old!</div>
+}
+
+User.propTypes ={
+  name: PropTypes.string
 }
 
 export default App;
