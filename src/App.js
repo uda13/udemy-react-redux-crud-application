832c4dbb9124103a97c0f901e2a3c5046ab87755
Osamu Uda <uda13@hotmail.co.jp>
2024-02-15 15:42:12 +0900

    learnm state and setState

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React ,{Component}  from 'react';
+
+const App = () => (<Counter></Counter>)
+
+class Counter extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {count : 0}
+  }
+
+  hundlePlusButton =() => {
+    this.setState({count: this.state.count + 1})
+  }
+
+  hundleMinusButton =() => {
+    this.setState({count: this.state.count - 1})
+  }
+ render(){
+  return (
+    <React.Fragment>
+    <div>count: {this.state.count}</div>
+    <button onClick={this.hundlePlusButton}>+1</button>
+    <button onClick={this.hundleMinusButton}>-1</button>
+    </React.Fragment>
+  )
+ }
+}
+
+export default App;
